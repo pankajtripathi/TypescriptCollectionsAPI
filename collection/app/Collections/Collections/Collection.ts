03bf07258455c04b7178c20bd5cd24e3931cc2f6
bhaskerpyadav@gmail.com <bhaskerpyadav@gmail.com>
2016-02-27 19:43:18 +0530

    Basic interface List, Map, Set with packaging

---
+++ collection/app/Collections/Collections/Collection.ts
@@ -0,0 +1,37 @@
+export interface Collection<E>{
+   
+   /*
+    * returns true if map conatins the supplied key, K.
+    */
+    add(e:E):boolean;
+    
+   /*
+    * clears the set and returns true is succeeds else returns false
+    */
+    clear():boolean;
+    
+   /*
+    * returns true is set contains e
+    */
+    contains(e:E):boolean;
+    
+   /*
+    * returns true if set is empty
+    */
+    isEmpty():boolean;
+    
+   /*
+    * removes object e from collection
+    */
+    remove(e:E):boolean;
+    
+   /*
+    * returns the size of the collection
+    */
+    size():number;
+    
+   /*
+    * returns an array containing the collection 
+    */
+    toArray():Array<E>;
+}
---
+++ collection/app/Collections/Map/Map.ts
@@ -0,0 +1,32 @@
+export interface Map<K,V>{
+    
+    /*
+     *Inserts value v, with key k and returns the stored value 
+     */
+    put(key:K,value:V):V;
+    
+   /*
+    * Clears map collection, returns true if succeeds in doing so else false 
+    */
+    clear():boolean;
+    
+   /*
+    * returns true if map conatins the supplied key, K.
+    */
+    containsKey(key:K):boolean;
+    
+   /*
+    * returns true if map contains the supplied value, V 
+    */    
+    containsValue(value:V):boolean;
+    
+   /*
+    * returns true if map is empty
+    */
+    isEmpty():boolean;
+    
+   /*
+    * returns the number of entries in map
+    */
+    size():number;
+}
